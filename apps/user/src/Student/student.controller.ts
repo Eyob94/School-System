d9a56a08d12c72c8cb63d91f3fd68ddb22633c89
Eyob94 <eyobsh123@gmail.com>
2023-06-02 00:30:26 +0300

    feat(school): :sparkles: initiated the school service

--- apps/user/src/Student/student.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('student')
-export class StudentController {}
--- apps/user/src/db/db.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { DatabaseService } from './db.service';
-
-@Global()
-@Module({
-  providers: [DatabaseService],
-  exports: [DatabaseService],
-})
-export class DatabaseModule {}
